ebba11c25f7192b21e7bdb1aed942e2019758adb
vankokos <vano9995@gmail.com>
2021-01-27 23:05:30 +0200

    init

---
+++ js/script1.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // slick carousel
+    $('.main__inner-top-right').slick({
+      dots: true,
+      vertical: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      verticalSwiping: true,
+      dotsClass:"vertical-dots",
+      arrows: false,
+    });
+  });
---
+++ js/script2.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // slick carousel
+    $('.reviews__inner-slider').slick({
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      arrows: false,
+      dotsClass:"slick-dots2"
+    });
+  });
